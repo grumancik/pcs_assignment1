bb9ca4fae018c97bf1a8819a41730eaec0a0fc5f
grumancik <genrumancik@gmail.com>
2013-10-05 18:28:53 -0700

    First Assignment

---
+++ Variables.js
@@ -0,0 +1,24 @@
+var tm = "the mouse ";
+var lt = "likes to ";
+var rq = "run quickly";
+var tc = "the cat ";
+var ca = "chase after ";
+var nom = "eat ";
+var chz = "pieces of cheese";
+
+var mouse = tm + lt + rq //'the mouse likes to run quickly'
+console.log(mouse)
+
+var catChase = tc + lt + ca + tm //'the cat likes to chase after the mouse'
+console.log(catChase)
+
+var a = 2
+var b = 3
+var c = 4
+var d = a + b
+var e = a + b + c
+console.log(d)
+console.log(e)
+
+var yummy = tm + lt + nom + a + chz //'the mouse likes to eat 2 pieces of cheese'
+console.log(yummy)
